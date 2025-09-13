3182ab6e9496c5334dfc6c9073ee1bfc1b8a1aad
Tyler Jones <tyler@tylerjones.io>
2025-09-12 20:38:37 -0600

    Revert nettle

--- scripts/source.sh
+++ scripts/source.sh
@@ -168,7 +168,7 @@ get_library_source() {
   #   ;;
   nettle)
     SOURCE_REPO_URL="https://git.lysator.liu.se/nettle/nettle"
-    SOURCE_ID="nettle_3.10.2_release_20250626"
+    SOURCE_ID="nettle_3.8.1_release_20220727"
     SOURCE_TYPE="TAG"
     ;;
   # opencore-amr)
