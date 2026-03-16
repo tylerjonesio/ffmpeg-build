08e415395f1b71236323374844c7269161e289a6
Tyler Jones <tyler@tylerjones.io>
2026-03-16 13:25:39 -0600

    Bump to 7.1.3

--- scripts/source.sh
+++ scripts/source.sh
@@ -33,7 +33,7 @@ get_library_source() {
   #   ;;
   ffmpeg)
     SOURCE_REPO_URL="https://github.com/FFmpeg/FFmpeg"
-    SOURCE_ID="n7.1.1"
+    SOURCE_ID="n7.1.3"
     SOURCE_TYPE="TAG"
     ;;
   # fontconfig)
