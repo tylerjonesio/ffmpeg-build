3aeae2bd938a0efeacfd9ce1c1512e0904de1bf9
Tyler Jones <tyler@tylerjones.io>
2026-03-23 18:32:52 -0600

    Fix license copy

--- scripts/function.sh
+++ scripts/function.sh
@@ -1832,7 +1832,7 @@ get_external_library_license_path() {
   30) echo "${BASEDIR}/src/$(get_library_name "$1")/COPYING.txt" ;;
   43) echo "${BASEDIR}/src/$(get_library_name "$1")/COPYRIGHT" ;;
   46) echo "${BASEDIR}/src/$(get_library_name "$1")/leptonica-license.txt" ;;
-  4 | 10 | 13 | 17 | 21 | 27 | 31 | 32 | 36 | 40 | 49) echo "${BASEDIR}/src/$(get_library_name "$1")/LICENSE" ;;
+  10 | 13 | 17 | 21 | 27 | 31 | 32 | 36 | 40 | 49) echo "${BASEDIR}/src/$(get_library_name "$1")/LICENSE" ;;
   *) echo "${BASEDIR}/src/$(get_library_name "$1")/COPYING" ;;
   esac
 }
